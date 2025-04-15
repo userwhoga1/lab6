4ca52cc847898c6d7dad5916f9136f270c85b6f3
Nail <nailbest001@gmail.com>
2025-04-15 12:16:23 +0500

    next

--- app/src/main/java/com/example/laba66/MainActivity.java
+++ app/src/main/java/com/example/laba66/MainActivity.java
@@ -1,24 +1,47 @@
 package com.example.laba66;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<Country> countryList = new ArrayList<>();
+        countryList.add(new Country("Vietnam", "98,000,000", R.drawable.vn));
+        countryList.add(new Country("United States", "320,000,000", R.drawable.usa));
+        countryList.add(new Country("Russia", "142,000,000", R.drawable.russia));
+
+        CountryAdapter adapter = new CountryAdapter(countryList, country -> {
+            String message = "Selected: " + country.getName() + " (Population: " + country.getPopulation() + ")";
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+
+
         });
+
+
+
+        recyclerView.setAdapter(adapter);
     }
 }
